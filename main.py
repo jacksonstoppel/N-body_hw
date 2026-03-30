4fabcdaf3f4f34602ed6d7dff87a704305d694f9
Jackson Stoppel <stoppeljackson@gmail.com>
2026-03-30 10:19:49 -0500

    finished prob 1 and 2

--- main.py
+++ main.py
@@ -1,36 +1,249 @@
 import math
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
 
+def make_orbit_movie(
+    x_vals, y_vals, t_vals,
+    filename="earth_orbit.gif",
+    fps=60,
+    skip=10,
+    euler_x_vals=None,
+    euler_y_vals=None,
+    euler_t_vals=None
+):
+    # Main trajectory (for example: leapfrog)
+    x_vals = np.asarray(x_vals)[::skip]
+    y_vals = np.asarray(y_vals)[::skip]
+    t_vals = np.asarray(t_vals)[::skip]
 
+    # Check whether Euler data was provided
+    include_euler = (
+        euler_x_vals is not None and
+        euler_y_vals is not None and
+        euler_t_vals is not None
+    )
 
-def leapfrog(initial_pos, initial_vel, acceleration_func, tot_time, tot_steps):
-    pos_mat_x = [initial_pos[0]]
-    pos_mat_y = [initial_pos[1]]
-    time_mat = [0]
-    time_step = tot_time/tot_steps
-    pos_x = initial_pos[0]
-    pos_y = initial_pos[1]
-    theta = math.atan(pos_y/pos_x)
-    v_12_x = initial_pos[1] + 0.5*acceleration_func(pos_x, pos_y, 1)*math.sin(theta)*time_step
-    v_12_y = initial_vel[1] + 0.5*acceleration_func(pos_x, pos_y, 1)*math.cos(theta)*time_step
-    i = 0
-    while i <= tot_steps:
-        theta = math.atan(pos_y/pos_x)
-        pos_x += v_12_x*time_step
-        pos_y += v_12_y*time_step
-        pos_mat_x.append(pos_x)
-        pos_mat_y.append(pos_y)
-        v_12_x += 0.5*acceleration_func(pos_x, pos_y, 1)*math.cos(theta)*time_step
-        v_12_y += 0.5*acceleration_func(pos_x, pos_y, 1)*math.sin(theta)*time_step
-        i+= 1
-        time_mat.append(i*time_step)
-    return pos_mat_x, pos_mat_y, time_mat
+    if include_euler:
+        euler_x_vals = np.asarray(euler_x_vals)[::skip]
+        euler_y_vals = np.asarray(euler_y_vals)[::skip]
+        euler_t_vals = np.asarray(euler_t_vals)[::skip]
 
-def gravity_func(x, y, m_1):
+        # Make sure both animations have the same number of frames
+        n_frames = min(len(x_vals), len(y_vals), len(t_vals),
+                       len(euler_x_vals), len(euler_y_vals), len(euler_t_vals))
+
+        x_vals = x_vals[:n_frames]
+        y_vals = y_vals[:n_frames]
+        t_vals = t_vals[:n_frames]
+        euler_x_vals = euler_x_vals[:n_frames]
+        euler_y_vals = euler_y_vals[:n_frames]
+        euler_t_vals = euler_t_vals[:n_frames]
+
+        all_x = np.concatenate([x_vals, euler_x_vals])
+        all_y = np.concatenate([y_vals, euler_y_vals])
+    else:
+        n_frames = min(len(x_vals), len(y_vals), len(t_vals))
+        x_vals = x_vals[:n_frames]
+        y_vals = y_vals[:n_frames]
+        t_vals = t_vals[:n_frames]
+
+        all_x = x_vals
+        all_y = y_vals
+
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.set_aspect('equal')
+
+    x_min, x_max = all_x.min(), all_x.max()
+    y_min, y_max = all_y.min(), all_y.max()
+
+    x_pad = 0.1 * (x_max - x_min if x_max > x_min else 1.0)
+    y_pad = 0.1 * (y_max - y_min if y_max > y_min else 1.0)
+
+    ax.set_xlim(x_min - x_pad, x_max + x_pad)
+    ax.set_ylim(y_min - y_pad, y_max + y_pad)
+
+    ax.set_xlabel("x [AU]")
+    ax.set_ylabel("y [AU]")
+    ax.set_title("Earth Orbit Around the Sun")
+
+    # Sun
+    ax.plot(0, 0, 'yo', markersize=10, label="Sun")
+
+    # Main orbit: leapfrog
+    orbit_line, = ax.plot([], [], '-', lw=1.5, label="Leapfrog")
+    earth_point, = ax.plot([], [], 'o', markersize=8)
+
+    # Optional Euler orbit
+    if include_euler:
+        euler_line, = ax.plot([], [], '--', lw=1.5, label="Euler")
+        euler_point, = ax.plot([], [], 's', markersize=6)
+
+    time_text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
+    ax.legend(loc="upper right")
+
+    def init():
+        orbit_line.set_data([], [])
+        earth_point.set_data([], [])
+
+        if include_euler:
+            euler_line.set_data([], [])
+            euler_point.set_data([], [])
+
+        time_text.set_text("")
+
+        if include_euler:
+            return orbit_line, earth_point, euler_line, euler_point, time_text
+        else:
+            return orbit_line, earth_point, time_text
+
+    def update(frame):
+        # Leapfrog
+        orbit_line.set_data(x_vals[:frame+1], y_vals[:frame+1])
+        earth_point.set_data([x_vals[frame]], [y_vals[frame]])
+
+        if include_euler:
+            euler_line.set_data(euler_x_vals[:frame+1], euler_y_vals[:frame+1])
+            euler_point.set_data([euler_x_vals[frame]], [euler_y_vals[frame]])
+
+            time_text.set_text(
+                f"t = {t_vals[frame]:.3f} years"
+            )
+
+            return orbit_line, earth_point, euler_line, euler_point, time_text
+        else:
+            time_text.set_text(f"t = {t_vals[frame]:.3f} years")
+            return orbit_line, earth_point, time_text
+
+    anim = FuncAnimation(fig, update, frames=n_frames, init_func=init, blit=True)
+    anim.save(filename, writer="pillow", fps=fps)
+    plt.close(fig)
+
+def gravity_accel(x, y, m_1=1):
     r = math.sqrt(x**2 + y**2)
-    return -(4*(math.pi**2)*m_1)/(r**2)
+    factor = -(4 * math.pi**2 * m_1) / (r**3)
+    return factor * x, factor * y
 
-x, y, t = leapfrog([1, 0], [0,0.00001], gravity_func, 10, 1000)
+def leapfrog(initial_pos, initial_vel, acceleration_func, tot_time, tot_steps, m_1=1):
+    x, y = initial_pos
+    vx, vy = initial_vel
+    dt = tot_time / tot_steps
 
-plt.plot(t, y)
+    pos_mat_x = [x]
+    pos_mat_y = [y]
+    vel_mat_x = [vx]
+    vel_mat_y = [vy]
+    time_mat = [0.0]
+
+    ax, ay = acceleration_func(x, y, m_1)
+
+    # half-step velocity
+    vx_half = vx + 0.5 * ax * dt
+    vy_half = vy + 0.5 * ay * dt
+
+    for i in range(tot_steps):
+        # full-step position
+        x += vx_half * dt
+        y += vy_half * dt
+
+        pos_mat_x.append(x)
+        pos_mat_y.append(y)
+        time_mat.append((i + 1) * dt)
+
+        # acceleration at new position
+        ax, ay = acceleration_func(x, y, m_1)
+
+        vel_mat_x.append(vx_half + 0.5 * ax * dt)
+        vel_mat_y.append(vy_half + 0.5 * ay * dt)
+        # full-step half-velocity update
+        vx_half += ax * dt
+        vy_half += ay * dt
+
+    return pos_mat_x, pos_mat_y, vel_mat_x, vel_mat_y, time_mat
+
+def euler_method(initial_pos, initial_vel, acceleration_func, tot_time, tot_steps, m_1=1):
+    x, y = initial_pos
+    vx, vy = initial_vel
+    dt = tot_time / tot_steps
+
+    pos_mat_x = [x]
+    pos_mat_y = [y]
+    vel_mat_x = [vx]
+    vel_mat_y = [vy]
+    time_mat = [0.0]
+
+    for i in range(tot_steps):
+        ax, ay = acceleration_func(x, y, m_1)
+
+        x += vx * dt
+        y += vy * dt
+        vx += ax * dt
+        vy += ay * dt
+
+        pos_mat_x.append(x)
+        pos_mat_y.append(y)
+        vel_mat_x.append(vx)
+        vel_mat_y.append(vy)
+        time_mat.append((i + 1) * dt)
+
+    return pos_mat_x, pos_mat_y, vel_mat_x, vel_mat_y, time_mat
+
+
+initial_pos = [1.0, 0.0]
+initial_vel = [0.0, 2*math.pi]
+
+x_l, y_l, vx_l, vy_l, t_l = leapfrog(initial_pos, initial_vel, gravity_accel, 3, 900)
+x_e, y_e, vx_e, vy_e, t_e = euler_method(initial_pos, initial_vel, gravity_accel, 3, 900)
+
+make_orbit_movie(x_l, y_l, t_l, euler_t_vals=t_e, euler_x_vals=x_e, euler_y_vals=y_e, filename="earth_orbit_with_euler_problem1.gif")
+
+v_leap = []
+v_euler = []
+e_euler = []
+e_leap = []
+for i in range(len(vx_l)):
+    v_leap.append(math.sqrt(vx_l[i]**2 + vy_l[i]**2))
+    v_euler.append(math.sqrt(vx_e[i]**2 + vy_e[i]**2))
+    e_euler.append(0.5*v_euler[i]**2 - (4*math.pi**2)/(math.sqrt(x_e[i]**2 + y_e[i]**2)))
+    e_leap.append(0.5*v_leap[i]**2 - (4*math.pi**2)/(math.sqrt(x_l[i]**2 + y_l[i]**2)))
+
+plt.plot(t_e, v_euler, label="Euler", color="red")
+plt.plot(t_l, v_leap, label="Leapfrog", color="blue")
+plt.xlabel('t(years)')
+plt.ylabel('v(AU/year)')
+plt.legend(loc=1)
+plt.show()
+
+plt.plot(t_e, e_euler, label="Euler", color="red")
+plt.plot(t_l, e_leap, label="Leapfrog", color="blue")
+plt.xlabel('t(years)')
+plt.ylabel('Energy')
+plt.legend(loc=1)
+plt.show()
+
+
+# Start of problem 2
+initial_pos = [1.0, 0.0]
+initial_vel = [0.0, 0.8 * (2 * math.pi)]
+
+x_l, y_l, vx_l, vy_l, t_l = leapfrog(initial_pos, initial_vel, gravity_accel, 5, 900)
+x_e, y_e, vx_e, vy_e, t_e = euler_method(initial_pos, initial_vel, gravity_accel, 5, 900)
+
+make_orbit_movie(x_l, y_l, t_l, euler_t_vals=t_e, euler_x_vals=x_e, euler_y_vals=y_e, filename="earth_orbit_with_euler_problem2.gif")
+
+v_leap = []
+v_euler = []
+e_euler = []
+e_leap = []
+for i in range(len(vx_l)):
+    v_leap.append(math.sqrt(vx_l[i]**2 + vy_l[i]**2))
+    v_euler.append(math.sqrt(vx_e[i]**2 + vy_e[i]**2))
+    e_euler.append(0.5*v_euler[i]**2 - (4*math.pi**2)/(math.sqrt(x_e[i]**2 + y_e[i]**2)))
+    e_leap.append(0.5*v_leap[i]**2 - (4*math.pi**2)/(math.sqrt(x_l[i]**2 + y_l[i]**2)))
+
+plt.plot(t_e, e_euler, label="Euler", color="red")
+plt.plot(t_l, e_leap, label="Leapfrog", color="blue")
+plt.xlabel('t(years)')
+plt.ylabel('Energy')
+plt.legend(loc=1)
 plt.show()
